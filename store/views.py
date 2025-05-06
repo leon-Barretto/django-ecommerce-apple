8d0722eb6c6e88d00ba327826872104e315c65dd
Leon <leon.barretto@uleth.ca>
2025-05-05 21:27:23 -0600

    added the watch

--- store/views.py
+++ store/views.py
@@ -16,7 +16,3 @@ def product_list(request):
     }
     return render(request, 'apple/apple_home.html', context)
 
-def ipad_detail(request):
-    return render(request, 'apple/ad-ipad.html')
-def iphone_detail(request):
-    return render(request, 'apple/ad-iphone.html')
